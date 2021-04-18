e0ed74e16736a6229886c21c7416ead63d855a38
Tulio Souza <tuliosouzasl2021@gmail.com>
2021-04-17 21:14:45 -0300

    Primeiro Projeto

---
+++ Exercicios-Js/Fundamentos/comentario.js
@@ -0,0 +1,21 @@
+// Comentário de uma linha
+console.log('Linha 1') // ou do lado
+
+/*
+Comentário de 
+múltiplas linhas
+*/
+console.log('Linha 2')
+
+/* 
+* Comentário de
+* múlplicas linhas
+*/ 
+console.log('Linha 3')
+
+/* 
+* 1 -> Evitem colocar comentarios obvios
+* 2 -> utilizem nomes de funções ou variaveis de facil entedimento (código limpo)
+* 3 -> Explicar se essa passa por muitos serviões e etc...
+*/
+
